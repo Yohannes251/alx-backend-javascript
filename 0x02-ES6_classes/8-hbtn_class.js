22ae3fde49a06f9bca3ab75bd0c00de34ddf8d1d
Yohannes251 <yohannes.z251@gmail.com>
2022-04-07 21:33:47 +0000

    Override primitive typecasting methods

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,16 @@
+export default class HolbertonClass {
+  constructor(size = 0, location = '') {
+    this._size = size;
+    this._location = location;
+  }
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') {
+      return this._size;
+    }
+    if (hint === 'string') {
+      return this._location;
+    }
+    return null;
+  }
+}

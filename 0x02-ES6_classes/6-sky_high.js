d3d8055c034901dfbb09aece2267a153ed49fecc
Yohannes251 <yohannes.z251@gmail.com>
2022-04-07 13:55:44 +0000

    eslint error fix

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,20 +1,21 @@
-import Building from './5-building.js'
+import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
-    this.floors = floors
+    this.floors = floors;
   }
 
   // Getter/setter for floors
   get floors() {
     return this._floors;
   }
+
   set floors(newFloors) {
     this._floors = newFloors;
   }
-  
+
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this.floors} floors`
+    return `Evacuate slowly the ${this.floors} floors`;
   }
 }

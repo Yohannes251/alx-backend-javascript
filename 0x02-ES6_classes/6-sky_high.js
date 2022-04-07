92e2e08b852bc7af22b6a04660ee47f96710df9a
Yohannes251 <yohannes.z251@gmail.com>
2022-04-07 13:53:00 +0000

    Inheritance

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,20 @@
+import Building from './5-building.js'
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this.floors = floors
+  }
+
+  // Getter/setter for floors
+  get floors() {
+    return this._floors;
+  }
+  set floors(newFloors) {
+    this._floors = newFloors;
+  }
+  
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`
+  }
+}

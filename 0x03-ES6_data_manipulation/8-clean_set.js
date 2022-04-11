62d16c749756e937abddc82e158dcd27bf5c2ad0
Yohannes251 <yohannes.z251@gmail.com>
2022-04-11 19:19:43 +0000

    Added a new condition

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,10 @@
 export default function cleanSet(set, startString) {
   const output = [];
+  if (startString.length === 0) {
+    return '';
+  }
   for (const str of set) {
-    if (str.startsWith(startString) === true && startString.length !== 0) {
+    if (str.startsWith(startString) === true) {
       output.push(str.slice(startString.length));
     }
   }

1afd02ffb7276030e978005a4bc0b4f6b75b5cec
Yohannes251 <yohannes.z251@gmail.com>
2022-04-13 02:37:32 +0000

    Added a new condition

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,4 +1,7 @@
 export default function updateUniqueItems(map1) {
+  if (map1 instanceof Map === false) {
+    throw Error('Cannot process');
+  }
   for (const key of map1.keys()) {
     if (map1.get(key) === 1) {
       try {

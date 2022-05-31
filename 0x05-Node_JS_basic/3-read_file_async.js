8fc83dc5bbed11b1e6984c2b52f7630a2077d51f
Yohannes251 <yohannes.z251@gmail.com>
2022-05-31 19:22:50 +0000

    Asynchronously read file

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+
+async function countStudents(path) {
+  if (!fs.existsSync(path)) {
+    throw Error('Cannot load the database');
+  }
+
+  const content = await fs.promises.readFile(path, 'utf8');
+  const contentArray = content.split('\n');
+
+  let iteration = 0;
+  const fields = [];
+  for (const line of contentArray) {
+    if (iteration > 0) {
+      if (line) {
+        fields.push(line.split(',')[3]);
+      }
+    }
+    iteration = 1;
+  }
+
+  console.log(`Number of students: ${fields.length}`);
+
+  const checked = [];
+  for (const field of fields) {
+    if (!checked.includes(field)) {
+      const nameList = [];
+      for (const line of contentArray) {
+        const data = line.split(',');
+        if (data[3] === field) {
+          nameList.push(data[0]);
+        }
+      }
+      checked.push(field);
+      console.log(`Number of students in ${field}: ${nameList.length}. List: ${nameList.join(', ')}`);
+    }
+  }
+}
+
+module.exports = countStudents;

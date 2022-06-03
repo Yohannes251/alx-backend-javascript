fb8876766a01520535d327a37904ebbbb181f242
Yohannes251 <yohannes.z251@gmail.com>
2022-06-03 15:04:18 +0000

    Fixed typo

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 async function readDatabase(path) {
   if (!fs.existsSync(path)) {
-    throw new Error('Can not load the database');
+    throw new Error('Cannot load the database');
   }
 
   const content = await fs.promises.readFile(path, 'utf8');
